56bc5604e0cd467f95d0e28488ca421bb92b43d8
yangwt <yangwenting@meizu.com>
2017-02-16 20:40:31 +0800

    update banner canvas

--- static/js/index.js
+++ static/js/index.js
@@ -61,51 +61,119 @@ window.cancelFrame = (function(){
 })();
 
 var init = function () {
-  initCanvas();
+  var canvas = new BannerCanvas('banner-canvas');
+  canvas.init();
 };
 
-var initCanvas = function () {
-  var canvas = document.getElementById('banner-canvas'),
-      ctx = canvas.getContext('2d'),
-      width = window.innerWidth,
+var BannerCanvas = function( _id ) {
+  this.canvas = document.getElementById( _id );
+  this.ctx = null;
+  
+  if ( this.canvas.getContext ) {
+    this.ctx = this.canvas.getContext('2d');
+  }
+  
+  var width = window.innerWidth,
       height = window.innerHeight;
 
-	canvas.width = width;
-	canvas.height = height;
+	this.canvas.width = width;
+	this.canvas.height = height;
+  
+  this.lines = [];
+};
+
+BannerCanvas.prototype = {
+  init: function () {
+    this.initLines();
+  },
+  initLines: function () {
+    var self = this,
+        count = 10;
+    
+    for (var i = 0; i < 10; i++) {
+      self.lines.push({
+        x: this.canvas.width + Math.random() * 100,
+        y: this.canvas.height * Math.random(),
+        length: (Math.random() + 1) * 150,
+        lineW: (Math.random() + 1) * 3,
+        speed: (Math.random() + 1) * 10
+      });
+    }
+    
+    self.startLineAnim();
+  },
+  startLineAnim: function () {
+    var self = this,
+        lines = self.lines;
+    
+    function render() {
+      self.clearCanvas();
+      for (var i = 0; i < lines.length; i++) {
+         self.drawLines(lines[i].x, lines[i].y, lines[i].length, lines[i].lineW);
+        lines[i].x -= lines[i].speed;
+        
+        if (lines[i].x < -lines[i].length) {
+          self.lines.splice(i, 1);
+          self.lines.push({
+            x: self.canvas.width + Math.random() * 100,
+            y: self.canvas.height * Math.random(),
+            length: (Math.random() + 1) * 150,
+            lineW: (Math.random() + 1) * 3,
+            speed: (Math.random() + 1) * 20
+          });  
+        }
+      }
       
-	var drawLine = function(x, y, len) {
-		
-		ctx.save();
-		ctx.translate(width/2, height/2);
-		ctx.beginPath();
-		ctx.moveTo(-len/2, 0);
-		ctx.lineTo(len/2, 0);
-		ctx.lineWidth=10;
-		ctx.strokeStyle="#0000ff";
+      window.requestFrame(render);
+    }
+    
+    render();
+  },
+  drawLines: function (_x, _y, _len, _lineWidth) {
+    var self = this,
+        ctx = self.ctx,
+        deg = -10 * Math.PI / 180;
+    
+    ctx.save();
+    ctx.rotate(-10*Math.PI/180);
+    ctx.translate(_x, _y);
+    ctx.beginPath();
+		ctx.moveTo(-_len/2, 0);
+		ctx.lineTo(_len/2, 0);
+		ctx.lineWidth = _lineWidth;
+		ctx.strokeStyle = "#0000ff";
 		ctx.stroke();
 		ctx.closePath();
 		ctx.restore();
 
 		ctx.save();
-		ctx.translate(width/2, height/2);
-		ctx.scale(4,.5);
+    ctx.rotate(-10*Math.PI/180);
+    ctx.translate(_x, _y);
+    ctx.scale(4,.3);
 		ctx.beginPath();
-
 		var grd=ctx.createRadialGradient(0,0,0,0,0,20);
 		grd.addColorStop(0,"rgba(255, 255, 255, .5)");
 		grd.addColorStop(1,"rgba(255, 255, 255, 0)");
 		ctx.arc(0, 0, 20, 0, 2*Math.PI);
-		ctx.fillStyle=grd;
+		ctx.fillStyle = grd;
 		ctx.fill();
 		ctx.closePath();
 		ctx.restore();
-	};
-
-	var clearCanvas = function() {
-		ctx.clearRect(0, 0, width, height);
-	};
-
-	drawLine(width/2, height/2, 200);
+    
+  },
+  translateToCenter: function () {
+    var self = this,
+        canvas = self.canvas,
+        ctx = self.ctx,
+        centerX = canvas.width / 2,
+        centerY = canvas.height / 2;
+    
+    ctx.translate(centerX, centerY);
+  },
+  clearCanvas: function () {
+    var self = this;
+    self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
+  }
 };
 
-// window.onload = init;
+window.onload = init;
